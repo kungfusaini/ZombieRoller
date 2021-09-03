664170a076915fc96fc08628110397a7b37f21a7
Sumeet Saini <sumeetsaini621@gmail.com>
2021-09-03 17:47:14 +0100

    initial commit

---
+++ Roll-A-Zombie/Assets/GameManager.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour{
+
+    private int selectedZombiePosition;
+    private int score = 0;
+    public Text scoreText;
+    public GameObject selectedZombie;
+    public List<GameObject> zombies;
+    public Vector3  selectedSize;
+    public Vector3 defaultSize;
+
+    // Start is called before the first frame update
+    void Start(){
+      SelectZombie(selectedZombie);
+      scoreText.text = "Score: " + score;
+    }
+
+    // Update is called once per frame
+    void Update(){
+
+      if (Input.GetKeyDown("left")){
+        getZombieLeft();
+      }
+
+      if (Input.GetKeyDown("right")){
+        getZombieRight();
+      }
+
+      if (Input.GetKeyDown("up")){
+        PushUp();
+      }
+    }
+
+    void getZombieLeft()
+    {
+      if (selectedZombiePosition == 0){
+        SelectZombie (zombies[3]);
+        selectedZombiePosition = 3;
+      }else {
+        GameObject newZombie = zombies[selectedZombiePosition - 1];
+        SelectZombie (newZombie);
+        selectedZombiePosition = selectedZombiePosition - 1;
+      }
+    }
+
+    void getZombieRight(){
+      if (selectedZombiePosition == 3){
+        SelectZombie (zombies[0]);
+        selectedZombiePosition = 0;
+      } else {
+        GameObject newZombie = zombies[selectedZombiePosition + 1];
+        SelectZombie (newZombie);
+        selectedZombiePosition = selectedZombiePosition + 1;
+
+      }
+    }
+
+    void SelectZombie(GameObject newZombie){
+      selectedZombie.transform.localScale = defaultSize;
+      selectedZombie = newZombie;
+      newZombie.transform.localScale = selectedSize;
+    }
+
+    void PushUp(){
+      Rigidbody rb  = selectedZombie.GetComponent<Rigidbody>();
+      rb.AddForce(0,0,10, ForceMode.Impulse);
+    }
+
+    public void AddPoint(){
+      score = score + 1;
+      scoreText.text = "Score: " + score;
+    }
+  }
---
+++ Roll-A-Zombie/Assets/Tiles.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class Tiles : MonoBehaviour
+{
+
+    public GameManager gameManager;
+    public AudioClip hit;
+    AudioSource source;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      source = GetComponent<AudioSource>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider enter){
+      gameManager.AddPoint();
+      source.PlayOneShot (hit);
+    }
+}
